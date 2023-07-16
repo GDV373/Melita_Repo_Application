a4bda843500b87f7a6a12ff38ea7a471ff15c058
GDV373 <42638883+GDV373@users.noreply.github.com>
2023-07-16 20:03:19 +0200

    refactored names

--- src/main/java/com/client/SpringCloundConfigClient/SpringCloudConfigClientApplication.java
+++ src/main/java/com/client/SpringCloundConfigClient/SpringCloudConfigClientApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCloundConfigClientApplication {
+public class SpringCloudConfigClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloundConfigClientApplication.class, args);
+		SpringApplication.run(SpringCloudConfigClientApplication.class, args);
 	}
 
 }
--- src/test/java/com/client/SpringCloundConfigClient/SpringCloudConfigClientApplicationTests.java
+++ src/test/java/com/client/SpringCloundConfigClient/SpringCloudConfigClientApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringCloundConfigClientApplicationTests {
+class SpringCloudConfigClientApplicationTests {
 
 	@Test
 	void contextLoads() {
